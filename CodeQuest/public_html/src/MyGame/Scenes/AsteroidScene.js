a603647755cda336e19c0108de1175bd7a1ebbcd
Douglas Winegarden <d3wine@gmail.com>
2018-04-25 15:56:50 -0700

    Fixed Raycast

--- CodeQuest/public_html/src/MyGame/Scenes/AsteroidScene.js
+++ CodeQuest/public_html/src/MyGame/Scenes/AsteroidScene.js
@@ -613,13 +613,80 @@ AsteroidScene.prototype.rayCast = function (p) {
                 //bottom right corner
                 thetaMax= -1*(Math.atan((astx + axf.getWidth()/2) / (asty-this.mHero.getXform().getYPos()-axf.getHeight()/2)));
             }                  
-            console.log(" theta: "+theta*180/Math.PI + " thetaMAx:"+thetaMax*180/Math.PI + " thetaMin"+thetaMin*180/Math.PI);
+            //console.log(" theta: "+theta*180/Math.PI + " thetaMAx:"+thetaMax*180/Math.PI + " thetaMin"+thetaMin*180/Math.PI);
+            
+            
+            // displaying Boundary rays
 
+                if(astx<=0){
+                    var rend = new Renderable();
+                    rend.setColor([1,0,0,1]);
+                    
+                    var toprx = astx + axf.getWidth()/2;
+                    var topry = asty + axf.getHeight()/2;
+                    
+                    
+                    rend.getXform().setPosition(toprx/2, topry/2 -30);
+                    rend.getXform().setSize(1,Math.sqrt(toprx*toprx+(topry+60)*(topry+60)));
+                    rend.getXform().setRotationInRad(thetaMin);
+                    
+                    //this.mAllObjs.addToSet(rend);
+                    
+                var rend2 = new Renderable();
+                    rend2.setColor([1,0,0,1]);  
+                    var blx = astx - axf.getWidth()/2;
+                    var bly = asty - axf.getHeight()/2;
+                    rend2.getXform().setPosition(blx/2, bly/2 -30);
+                    rend2.getXform().setSize(1,Math.sqrt(blx*blx+(bly+60)*(bly+60)));
+                    rend2.getXform().setRotationInRad(thetaMax);
+                    
+                    //this.mAllObjs.addToSet(rend2);
+                }
+                else{
+                    var rend = new Renderable();
+                    rend.setColor([1,0,0,1]);
+                    
+                    var tlx = astx - axf.getWidth()/2;
+                    var tly = asty + axf.getHeight()/2;
+                    
+                    rend.getXform().setPosition(tlx/2, tly/2 -30);
+                    rend.getXform().setSize(1,Math.sqrt(tlx*tlx+(tly+60)*(tly+60)));
+                    rend.getXform().setRotationInRad(thetaMin);
+                    
+                    //this.mAllObjs.addToSet(rend);
+                    
+                    var rend2 = new Renderable();
+                    rend2.setColor([1,0,0,1]);
+                    
+                    var brx = astx + axf.getWidth()/2;
+                    var bry = asty - axf.getHeight()/2;
+                                        
+                    rend2.getXform().setPosition(brx/2, bry/2 -30);
+                    rend2.getXform().setSize(1,Math.sqrt(brx*brx+(bry+60)*(bry+60)));
+                    rend2.getXform().setRotationInRad(thetaMax);
+                    
+                    //this.mAllObjs.addToSet(rend2);  
+                }
+            
+            
             //check if in possible theta range for collision
-            if(Math.abs(theta)>=Math.abs(thetaMin) && Math.abs(theta) <=Math.abs(thetaMax)){
-              console.log("Ray Hit! SelectionIndex:"+this.selectIndex+" asteroidDataType:"+ast.dataType);
-              this.procHit(ast, p);
+            if(theta>0){
+                if(theta>=thetaMin && theta <=thetaMax){
+                  console.log("Ray Hit! SelectionIndex:"+this.selectIndex+" asteroidDataType:"+ast.dataType);
+                  console.log(" theta: "+theta*180/Math.PI + " thetaMAx:"+thetaMax*180/Math.PI + " thetaMin"+thetaMin*180/Math.PI);
+                  this.procHit(ast, p);
+                }
             }
+            else{
+                
+                if(theta<=thetaMin && theta >=thetaMax){
+                  console.log("Ray Hit! SelectionIndex:"+this.selectIndex+" asteroidDataType:"+ast.dataType);
+                  console.log(" theta: "+theta*180/Math.PI + "  thetaMAx: "+thetaMax*180/Math.PI + " thetaMin"+thetaMin*180/Math.PI);
+                  this.procHit(ast, p);
+                }
+            }
+            
+            
           }
     }  
 }
