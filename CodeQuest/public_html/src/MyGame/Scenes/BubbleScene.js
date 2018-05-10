aa9aa4cc2b7ffe14c41b990795b69a227e14afa9
jgnewton <jeremygnewton@gmail.com>
2018-05-09 20:42:49 -0700

    Buub Scene

--- CodeQuest/public_html/src/MyGame/Scenes/BubbleScene.js
+++ CodeQuest/public_html/src/MyGame/Scenes/BubbleScene.js
@@ -129,6 +129,7 @@ function BubbleScene() {
     
     this.Ray=null;
     this.myBubbles = null;
+    this.myFlyBubble=null;
 }
 gEngine.Core.inheritPrototype(BubbleScene, Scene);
 
@@ -291,11 +292,13 @@ BubbleScene.prototype.initialize = function () {
     
     this.mCannon.intRotByDeg(0.01);
     
-    this.mNextBubble = new Bubble(this.mMeteorSprite, 0, -60, false, 0);
+    this.mNextBubble = new Bubble(this.mMeteorSprite, 0, -100, false, 0);
     
     this.mAllObjs.addToSet((this.mNextBubble));
     
     this.initBubbles();
+    
+    this.nextColor=0;
 };
 
 // This is the draw function, make sure to setup proper drawing environment, and more
@@ -383,6 +386,11 @@ BubbleScene.prototype.update = function () {
 
     this.mCannon.update();
     this.revealTime--;
+    
+    if(this.mFlyBubble!=null){
+        console.log("checking");
+        this.checkCollisions();
+    }
         
 };
 
@@ -652,132 +660,20 @@ BubbleScene.prototype.generateProjectile = function () {
     var yv = this.maxV*Math.cos(rot);
     
     
-    var color;
-    var b = new Bubble(this.mMeteorSprite, xp, yp, false, 0);
+    var b = new Bubble(this.mMeteorSprite, xp, yp, false, this.nextColor);
+    this.nextColor = Math.round(Math.random()*5);
+    
+    this.mNextBubble.color = this.nextColor;
+    this.mNextBubble.setColor();
        
     b.velocity(xv,yv); 
+    
+    this.mFlyBubble=b;
     
     this.myBubbles.addToSet(b);
 }
 
 
-//checking for raycast collisions
-BubbleScene.prototype.rayCast = function (p) {
-    console.log("raycast");
-    
-    for (var i = 0; i < this.mAllObjs.size(); i++) {
-      
-        var ast= this.mAllObjs.getObjectAt(i);
-        
-        if(ast instanceof Asteroid){
-
-            var axf = ast.getXform();
-            var astx = axf.getXPos();
-            var asty = axf.getYPos();
-
-            //the Actual Rotation of the Hero.
-            var theta = this.mCannon.cannon.getXform().getRotationInRad();                              
-
-            //ray to far bottom corner
-            var thetaMax=0;
-
-            //ray to near top corner
-            var thetaMin=0;
-
-            //case 1: Asteroid to left  (0 degrees is straight up, horizontal left is 90, horizontal right in -90...don't Ask... ask Kelvin              
-            if(astx<=0){
-                //top right
-                thetaMin= Math.abs(Math.atan((astx + axf.getWidth()/2) / (asty-this.mCannon.base.getXform().getYPos()+axf.getHeight()/2)));
-
-             //bottom left
-                thetaMax= Math.abs(Math.atan((astx - axf.getWidth()/2) / (asty-this.mCannon.base.getXform().getYPos()-axf.getHeight()/2)));
-            }
-
-            //asteroid on right
-            else{
-                //top left corner
-                thetaMin= -1*(Math.atan((astx - axf.getWidth()/2) / (asty-this.mCannon.base.getXform().getYPos()+axf.getHeight()/2)));
-
-                //bottom right corner
-                thetaMax= -1*(Math.atan((astx + axf.getWidth()/2) / (asty-this.mCannon.base.getXform().getYPos()-axf.getHeight()/2)));
-            }                  
-            //console.log(" theta: "+theta*180/Math.PI + " thetaMAx:"+thetaMax*180/Math.PI + " thetaMin"+thetaMin*180/Math.PI);
-            
-            
-            // displaying Boundary rays
-
-                if(astx<=0){
-                    var rend = new Renderable();
-                    rend.setColor([1,0,0,1]);
-                    
-                    var toprx = astx + axf.getWidth()/2;
-                    var topry = asty + axf.getHeight()/2;
-                    
-                    
-                    rend.getXform().setPosition(toprx/2, topry/2 -30);
-                    rend.getXform().setSize(1,Math.sqrt(toprx*toprx+(topry+60)*(topry+60)));
-                    rend.getXform().setRotationInRad(thetaMin);
-                    
-                    //this.mAllObjs.addToSet(rend);
-                    
-                var rend2 = new Renderable();
-                    rend2.setColor([1,0,0,1]);  
-                    var blx = astx - axf.getWidth()/2;
-                    var bly = asty - axf.getHeight()/2;
-                    rend2.getXform().setPosition(blx/2, bly/2 -30);
-                    rend2.getXform().setSize(1,Math.sqrt(blx*blx+(bly+60)*(bly+60)));
-                    rend2.getXform().setRotationInRad(thetaMax);
-                    
-                    //this.mAllObjs.addToSet(rend2);
-                }
-                else{
-                    var rend = new Renderable();
-                    rend.setColor([1,0,0,1]);
-                    
-                    var tlx = astx - axf.getWidth()/2;
-                    var tly = asty + axf.getHeight()/2;
-                    
-                    rend.getXform().setPosition(tlx/2, tly/2 -30);
-                    rend.getXform().setSize(1,Math.sqrt(tlx*tlx+(tly+60)*(tly+60)));
-                    rend.getXform().setRotationInRad(thetaMin);
-                    
-                    //this.mAllObjs.addToSet(rend);
-                    
-                    var rend2 = new Renderable();
-                    rend2.setColor([1,0,0,1]);
-                    
-                    var brx = astx + axf.getWidth()/2;
-                    var bry = asty - axf.getHeight()/2;
-                                        
-                    rend2.getXform().setPosition(brx/2, bry/2 -30);
-                    rend2.getXform().setSize(1,Math.sqrt(brx*brx+(bry+60)*(bry+60)));
-                    rend2.getXform().setRotationInRad(thetaMax);
-                    
-                    //this.mAllObjs.addToSet(rend2);  
-                }
-            
-            
-            //check if in possible theta range for collision
-            if(theta>0){
-                if(theta>=thetaMin && theta <=thetaMax){
-                  console.log("Ray Hit! SelectionIndex:"+this.selectIndex+" asteroidDataType:"+ast.dataType);
-                  console.log(" theta: "+theta*180/Math.PI + " thetaMAx:"+thetaMax*180/Math.PI + " thetaMin"+thetaMin*180/Math.PI);
-                  this.procHit(ast, p);
-                }
-            }
-            else{
-                
-                if(theta<=thetaMin && theta >=thetaMax){
-                  console.log("Ray Hit! SelectionIndex:"+this.selectIndex+" asteroidDataType:"+ast.dataType);
-                  console.log(" theta: "+theta*180/Math.PI + "  thetaMAx: "+thetaMax*180/Math.PI + " thetaMin"+thetaMin*180/Math.PI);
-                  this.procHit(ast, p);
-                }
-            }
-            
-            
-          }
-    }  
-}
 
 BubbleScene.prototype.procHit = function(obj, proj) {   
     //for making reveal message
@@ -822,3 +718,15 @@ BubbleScene.prototype.procHit = function(obj, proj) {
     }
     
 };
+
+BubbleScene.prototype.checkCollisions = function() {   
+    for(var i =0; i < this.myBubbles.size();i++){
+        var b = this.myBubbles.getObjectAt(i);
+        
+        var result = this.mFlyBubble.checkCollision(b);
+        
+        if(result){
+            console.log("collision");
+        }
+    }
+}
